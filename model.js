4124d4228a1415ad9440a8b7122b7ab891ffb7cd
Marcibouz <marcio_holz@gmx.de>
2025-01-23 23:27:49 +0100

    added artist name to info and changed style

--- model.js
+++ model.js
@@ -43,14 +43,14 @@ export const artistPictures = [
 ]
 
 export const infoTexts = [
-    'Titel: Der Tod des Sokrates' + '\n' + 'Jahr: 1787' + '\n' + 'Originalgröße: 129,5 x 196,2cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Metropolitan Museum of Art, New York',
-    'Titel: Der Wanderer über dem Nebelmeer' + '\n' + 'Jahr: 1818' + '\n' + 'Originalgröße: 94,8 x 74,8cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Hamburger Kunsthalle, Hamburg',
-    'Titel: Ein Sonntagnachmittag auf der Insel La Grande Jatte' + '\n' + 'Jahr: 1884-1886' + '\n' + 'Originalgröße: 207,6 x 308cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Art Institute of Chicago',
-    'Titel: Die große Welle vor Kanagawa' + '\n' + 'Jahr: 1830-1832' + '\n' + 'Originalgröße: 25 x 37cm' + '\n' + 'Medium: Japanischer Farbholzschnitt',
-    'Titel: Mona Lisa' + '\n' + 'Jahr: 1503-1506' + '\n' + 'Originalgröße: 77 x 53cm' + '\n' + 'Medium: Öl auf Pappelholz' + '\n' + 'Ausstellungsort: Musée du Louvre, Paris',
-    'Titel: Among the Sierra Nevada, California' + '\n' + 'Jahr: 1868' + '\n' + 'Originalgröße: 180 x 305,12cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Smithsonian American Art Museum, Washington, D.C.',
-    'Titel: Dienstmagd mit Milchkrug' + '\n' + 'Jahr: 1658-1660' + '\n' + 'Originalgröße: 45,5 x 41cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Rijksmuseum Amsterdam',
-    'Titel: Sternennacht' + '\n' + 'Jahr: 1889' + '\n' + 'Originalgröße: 73,7 x 92,1cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: MoMA, New York City'
+    'Künstler: ' + artists[0] + '\n' + 'Titel: Der Tod des Sokrates' + '\n' + 'Jahr: 1787' + '\n' + 'Originalgröße: 129,5 x 196,2cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Metropolitan Museum of Art, New York',
+    'Künstler: ' + artists[1] + '\n' + 'Titel: Der Wanderer über dem Nebelmeer' + '\n' + 'Jahr: 1818' + '\n' + 'Originalgröße: 94,8 x 74,8cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Hamburger Kunsthalle, Hamburg',
+    'Künstler: ' + artists[2] + '\n' + 'Titel: Ein Sonntagnachmittag auf der Insel La Grande Jatte' + '\n' + 'Jahr: 1884-1886' + '\n' + 'Originalgröße: 207,6 x 308cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Art Institute of Chicago',
+    'Künstler: ' + artists[3] + '\n' + 'Titel: Die große Welle vor Kanagawa' + '\n' + 'Jahr: 1830-1832' + '\n' + 'Originalgröße: 25 x 37cm' + '\n' + 'Medium: Japanischer Farbholzschnitt',
+    'Künstler: ' + artists[4] + '\n' + 'Titel: Mona Lisa' + '\n' + 'Jahr: 1503-1506' + '\n' + 'Originalgröße: 77 x 53cm' + '\n' + 'Medium: Öl auf Pappelholz' + '\n' + 'Ausstellungsort: Musée du Louvre, Paris',
+    'Künstler: ' + artists[5] + '\n' + 'Titel: Among the Sierra Nevada, California' + '\n' + 'Jahr: 1868' + '\n' + 'Originalgröße: 180 x 305,12cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Smithsonian American Art Museum, Washington, D.C.',
+    'Künstler: ' + artists[6] + '\n' + 'Titel: Dienstmagd mit Milchkrug' + '\n' + 'Jahr: 1658-1660' + '\n' + 'Originalgröße: 45,5 x 41cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: Rijksmuseum Amsterdam',
+    'Künstler: ' + artists[7] + '\n' + 'Titel: Sternennacht' + '\n' + 'Jahr: 1889' + '\n' + 'Originalgröße: 73,7 x 92,1cm' + '\n' + 'Medium: Öl auf Leinwand' + '\n' + 'Ausstellungsort: MoMA, New York City'
 ]
 
 export const arrows = [
